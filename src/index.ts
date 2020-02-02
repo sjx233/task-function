2a593eeacc6c80aa5486d6dc41ccd8538b416a0f
undefined <sjx233@qq.com>
2020-02-02 15:20:10 +0800

    better way to construct tasks

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,48 @@
-import { MinecraftFunction, Pack } from "minecraft-packs";
+import { DataPack } from "minecraft-packs";
 import ResourceLocation = require("resource-location");
 
 export class Task {
   private readonly commands: string[] = [];
-  public readonly functionId: ResourceLocation;
+  public readonly id: ResourceLocation;
 
-  public constructor(public readonly group: TaskGroup, public readonly id: number) {
-    this.functionId = new ResourceLocation("taskfunction", `${group.name}/${id}`);
+  public constructor(id: string | ResourceLocation) {
+    this.id = ResourceLocation.from(id);
   }
 
   public thenRun(task: Task | ResourceLocation, time?: number): this;
   public thenRun(command: string): this;
   public thenRun(task: Task | ResourceLocation | string, time = 0): this {
-    if (typeof task === "string") this.commands.push(task);
-    else {
-      const functionId = task instanceof ResourceLocation ? task.toString() : task.functionId;
-      this.commands.push((time = Math.round(time)) > 0 ? `schedule function ${functionId} ${time} append` : `function ${functionId}`);
+    if (typeof task === "string") {
+      this.commands.push(task);
+      return this;
     }
+    if (task instanceof Task) task = task.id;
+    time = Math.round(time);
+    this.commands.push(time > 0 ? `schedule function ${task} ${time} append` : `function ${task}`);
     return this;
   }
 
-  public addTo(pack: Pack): void {
-    pack.addResource(new MinecraftFunction(this.functionId, this.commands));
+  public addTo(pack: DataPack): void {
+    pack.functions.set(this.id, Array.from(this.commands));
   }
 }
 
 export class TaskGroup {
   private readonly tasks: Task[] = [];
+  public readonly id: ResourceLocation;
 
-  public constructor(public readonly name: string) {
-    if (!/^[a-z0-9/_.-]*$/.test(name)) throw new SyntaxError("Invalid name: non [a-z0-9/_.-] character");
-  }
-
-  public getTasks(): Task[] {
-    return Array.from(this.tasks);
-  }
-
-  public taskCount(): number {
-    return this.tasks.length;
+  public constructor(id: string | ResourceLocation) {
+    id = ResourceLocation.from(id);
+    this.id = ["", "/"].includes(id.path.slice(-1)) ? id : new ResourceLocation(id.namespace, id.path + "/");
   }
 
   public newTask(): Task {
-    const tasks = this.tasks;
-    const task = new Task(this, tasks.length);
-    tasks.push(task);
+    const task = new Task(new ResourceLocation(this.id.namespace, this.id.path + this.tasks.length));
+    this.tasks.push(task);
     return task;
   }
 
-  public addTo(pack: Pack): void {
+  public addTo(pack: DataPack): void {
     for (const task of this.tasks)
       task.addTo(pack);
   }
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,8 @@
-import { Pack, PackType } from "minecraft-packs";
+import { DataPack } from "minecraft-packs";
 import { TaskGroup } from "../index";
 
-const pack = new Pack(PackType.DATA_PACK, "Test datapack.");
-const taskGroup = new TaskGroup("group");
+const pack = new DataPack("test data pack");
+const taskGroup = new TaskGroup("taskfunction:test");
 const task1 = taskGroup.newTask()
   .thenRun("tellraw @a \"Task 1 Test 1\"")
   .thenRun("tellraw @a \"Task 1 Test 2\"")
@@ -18,7 +18,7 @@ taskGroup.newTask()
   .thenRun("tellraw @a \"Task 3 Test 1\"")
   .thenRun("tellraw @a \"Task 3 Test 2\"")
   .thenRun(task2, 0)
-  .thenRun(task2, 8)
+  .thenRun(task1, 8)
   .thenRun("tellraw @a \"Task 3 Test 3\"");
 taskGroup.addTo(pack);
 pack.write("test");
